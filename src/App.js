5cc48f22ef7bdc005cc33b8c83d400fd5fb2d9f7
Timothy Merfry Tiwow <s21810014@student.unklab.ac.id>
2021-11-29 22:13:14 +0800

    implemented displaying basic data taken from API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
 function App() {
+  const [data, setData] = useState("")
+
+  useEffect(
+    () => {
+      fetch("http://merfry.unaux.com/api/forKelasFrontEnd")
+        .then(resp => console.log(resp));
+    }, 
+  [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      testing
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
