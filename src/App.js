020dcac874666c6686ebdf92e53799f85675cee7
Timothy Merfry Tiwow <74723356+S21810014@users.noreply.github.com>
2021-11-29 22:15:08 +0800

    implemented displaying data from API (#4)
    * implemented displaying basic data taken from API
    
    * implemented displaying basic data taken from API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import { Typography } from '@mui/material'
+import React, { useEffect, useState } from 'react'
+import { HashRouter, Route, Routes } from 'react-router-dom'
+import About from './About'
+import Details from './Details'
+import Home from './Home'
 
 function App() {
+  const [data, setData] = useState(null)
+
+  useEffect(() => {
+      const myFunc = async () => {
+        // console.log("fetching data")
+
+        await fetch("https://merfry0309.ip6dns.xyz/api/proxy/covidindo/")
+          .then(resp => {
+            // console.log("data received, converting to json")
+
+            return resp.json()
+          })
+          .then(json => {
+            // console.log('data converted to json, preprocessing')
+
+            const temp = json.data.map((el, idx) => ({index: idx, ...el}))
+
+            json.data = temp
+            setData(json)
+          })
+      }
+
+      myFunc()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{
+      width: '100%', height: window.innerHeight 
+    }}>
+      {
+        !data ? 
+          <div style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '100%'
+          }}>
+            <Typography variant='h3' component='div'>
+              Loading Data...   
+            </Typography>
+          </div>
+          :
+          <HashRouter>
+            <Routes>
+              <Route exact path='/' element={
+                <Home apiData={data}/>
+              }/>
+              <Route exact path='/about' element={
+                <About/>
+              }/>
+              <Route exact path='/details/:id' element={
+                <Details/>
+              }/>
+            </Routes>
+          </HashRouter>
+    }
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/Details.js
@@ -0,0 +1,19 @@
+import { Typography } from '@mui/material'
+import React from 'react'
+
+function Details() {
+    return (
+        <div style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '100%'
+        }}>
+            <Typography variant='h3' component='div'>
+                Details Page  
+            </Typography>
+        </div>
+    )
+}
+
+export default Details
